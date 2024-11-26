fc0035a1da706d0604a39d75fc549fc2a307ecae
Josh-da-kid <carmenjosh84@gmail.com>
2024-11-26 17:34:57 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+const readline = require('readline');
 
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+const responses = {
+    Rock : [
+        "You failed!! computer chose paper",
+         "You won!! computer chose scissors",
+          "It's a tie"
+        ],
+    Paper: [
+        "You won!! computer chose rock",
+         "You failed!! computer chose scissors",
+          "Its a tie"
+        ],
+    Scissors: [
+        "You won!! computer chose paper",
+         "A tie!!"
+        ]
+       }
+
+rl.question("let's play....Rock, Paper or scissors? ", (name) => {
+    const feedback = responses[Math.floor(Math.random() * responses.length)];
+  console.log(feedback);
+  rl.close();
+});
