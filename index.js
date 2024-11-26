9283d4e888f837334c95d70702d68aeac5b60743
Sono Dennis Willbrand <so.willbrand@gmail.com>
2024-11-26 21:13:02 +0100

    use name from the parameters

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
-const readline = require('readline');
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 const responses = {
-    Rock : [
-        "You failed!! computer chose paper",
-         "You won!! computer chose scissors",
-          "It's a tie"
-        ],
-    Paper: [
-        "You won!! computer chose rock",
-         "You failed!! computer chose scissors",
-          "Its a tie"
-        ],
-    Scissors: [
-        "You won!! computer chose paper",
-         "A tie!!"
-        ]
-       }
+  Rock: [
+    "You failed!! computer chose paper",
+    "You won!! computer chose scissors",
+    "It's a tie",
+  ],
+  Paper: [
+    "You won!! computer chose rock",
+    "You failed!! computer chose scissors",
+    "Its a tie",
+  ],
+  Scissors: ["You won!! computer chose paper", "A tie!!"],
+};
 
 rl.question("let's play....Rock, Paper or scissors? ", (name) => {
-    const feedback = responses[Math.floor(Math.random() * responses.length)];
+  const feedback = responses[name].at(
+    Math.floor(Math.random() * responses[name].length)
+  );
   console.log(feedback);
   rl.close();
 });
